04d6fb3cca3c40f65882845ad456f3e638cdc889
Ryan <ryannichpurba44@gmail.com>
2025-08-03 19:09:24 +0700

    No IndoBERT first to deploy

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ def upload_resume():
         traceback.print_exc()  # Log error details to console
         return jsonify({"Error": str(e)}), 500
 
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))  # gunakan PORT dari Render
-    app.run(host='0.0.0.0', port=port)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT is not set
+    app.run(host="0.0.0.0", port=port)
 
